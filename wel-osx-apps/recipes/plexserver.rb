69c7b3dd686616ff75a23ae6d061b85578b7a328
Chris <chris@wideeyelabs.com>
2013-05-05 21:22:54 -0400

    Add plex server recipe
    This commit adds a recipe for plex server.

---
+++ wel-osx-apps/recipes/plexserver.rb
@@ -0,0 +1,7 @@
+dmg_package "Plex Media Server" do
+  dmg_name "PlexMediaServer-0.9.7.22.511-4b5280f-OSX.dmg"
+  source "http://plex.r.worldssl.net/PlexMediaServer/0.9.7.22.511-4b5280f/PlexMediaServer-0.9.7.22.511-4b5280f-OSX.dmg"
+  checksum "25ba43ef1a9510f1608d0435c727dd16ceaf0ce193cd559f599b4b9c06bce4ce"
+  action :install
+  owner WS_USER
+end
