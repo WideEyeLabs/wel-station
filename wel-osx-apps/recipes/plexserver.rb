901380094751ce259422fad26376e5f66eec8acd
Chris <chris@wideeyelabs.com>
2013-05-07 09:20:23 -0700

    Add volume directory for Plex Server
    This commit adds the volume directory for the plex server recipe.

--- wel-osx-apps/recipes/plexserver.rb
+++ wel-osx-apps/recipes/plexserver.rb
@@ -1,4 +1,5 @@
 dmg_package "Plex Media Server" do
+  volumes_dir "PlexMediaServer"
   dmg_name "PlexMediaServer-0.9.7.22.511-4b5280f-OSX.dmg"
   source "http://plex.r.worldssl.net/PlexMediaServer/0.9.7.22.511-4b5280f/PlexMediaServer-0.9.7.22.511-4b5280f-OSX.dmg"
   checksum "25ba43ef1a9510f1608d0435c727dd16ceaf0ce193cd559f599b4b9c06bce4ce"
