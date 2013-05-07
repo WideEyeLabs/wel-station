a6bb661d0f1e31edc941e0d53328e492a69606a9
Chris <chris@wideeyelabs.com>
2013-05-07 15:10:20 -0400

    Add Viber recipe
    This commit adds the viber recipe.

---
+++ wel-osx-apps/recipes/viber.rb
@@ -0,0 +1,6 @@
+dmg_package "Viber" do
+  owner WS_USER
+  source "http://download.cdn.viber.com/cdn/desktop/mac/Viber.dmg"
+  checksum "816e3cdfc659f29586cc4d503ab72a87dae42fe48fe29efbc49f0b86f0c1119e"
+  action :install
+end
