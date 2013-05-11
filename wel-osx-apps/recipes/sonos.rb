b13907ef7f4597543d9ede2a779ee037751dfbfe
Chris <chris@wideeyelabs.com>
2013-05-11 15:15:27 -0400

    Add eula accept
    This commit adds a eula accept for sonos recipe.

--- wel-osx-apps/recipes/sonos.rb
+++ wel-osx-apps/recipes/sonos.rb
@@ -3,5 +3,6 @@ dmg_package "Sonos" do
   owner WS_USER
   source "http://update.sonos.com/software/mac/mdcr/SonosDesktopController40.dmg"
   checksum "d9b722d9287e01338e5e291f22ec0ff5c5203e011dd4fb56cba8e5f8e4849402"
+  accept_eula true
   action :install
 end
