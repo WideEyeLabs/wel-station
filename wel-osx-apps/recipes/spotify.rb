cdd7f2086abbaa55fa8122d9f30c3ec6f1ca2183
Chris <chris@wideeyelabs.com>
2013-05-10 22:03:38 -0400

    Add Spotify recipe
    This commit adds the Spotify recipe.

---
+++ wel-osx-apps/recipes/spotify.rb
@@ -0,0 +1,26 @@
+unless File.exists?("/Applications/Spotify.app")
+
+  remote_file "#{Chef::Config[:file_cache_path]}/spotify.zip" do
+    source "http://download.spotify.com/SpotifyInstaller.zip"
+    owner WS_USER
+    checksum "5771c5300c2e8ca808c61a5a29505637d872e8d546b758361a6ca50cf2500bbb"
+  end
+
+  execute "unzip spotify" do
+    command "unzip -o #{Chef::Config[:file_cache_path]}/spotify.zip -d #{Chef::Config[:file_cache_path]}/"
+    user WS_USER
+  end
+
+  execute "copy spotify to /Applications" do
+    command "mv #{Chef::Config[:file_cache_path]}/Spotify.app #{Regexp.escape("/Applications/Spotify.app")}"
+    user WS_USER
+    group "admin"
+  end
+
+  ruby_block "test to see if Spotify.app was installed" do
+    block do
+      raise "Spotify.app was not installed" unless File.exists?("/Applications/Spotify.app")
+    end
+  end
+
+end
