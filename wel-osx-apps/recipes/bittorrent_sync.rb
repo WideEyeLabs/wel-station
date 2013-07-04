573cecb7f5d386949558f6d3bf6b806d54486a18
Chris <chris@wideeyelabs.com>
2013-07-04 14:05:15 -0400

    Fix bittorrent app name
    This commit fixes the app name for bittorrent sync.

--- wel-osx-apps/recipes/bittorrent_sync.rb
+++ wel-osx-apps/recipes/bittorrent_sync.rb
@@ -1,4 +1,4 @@
-dmg_package "BTSync" do
+dmg_package "BitTorrent Sync" do
   volumes_dir "BTSync"
   source "http://btsync.s3-website-us-east-1.amazonaws.com/BTSync.dmg"
   checksum "98a8a7a6aed21d173bf0e8b06d0ff30dcd807550837e41e72670affa8bdd71b6"
