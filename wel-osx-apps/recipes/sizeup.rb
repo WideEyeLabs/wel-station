d49ffc856e5e7f45e159cc2278fe442d8f22dd1b
Chris <chris@wideeyelabs.com>
2013-07-04 22:06:00 -0400

    Fix sizeup url
    This commit fixes the url to download sizeup from.

--- wel-osx-apps/recipes/sizeup.rb
+++ wel-osx-apps/recipes/sizeup.rb
@@ -1,5 +1,5 @@
 zip_package "SizeUp" do
-  source "http://www.irradiatedsoftware.com/downloads/?file=SizeUp.zip"
+  source "http://www.irradiatedsoftware.com/download/SizeUp.zip"
   checksum "8eee88fbe6cfc9b298c6c0e44092549d595306d29466320b8237b3ea88e70395"
   action :install
   owner WS_USER
